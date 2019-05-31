df9500a9d8e77ce86ed8fba1030b4a856d78e5d1
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-05-31 00:31:39 -0400

    added photos and beginner HTML and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// VARIABLES
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+
+// Declares an empty array to hold our guesses
+
+var guessesSoFar = [];
+
+// Declares a list of possible words
+
+var possibleWords = ["Mariah", "Whitney", "Celine", "Adele", "Madonna", "Rihanna", "Beyonce", "Gaga"]
+
+// Empty array holding the computer guess
+var computerGuess = "";
+
